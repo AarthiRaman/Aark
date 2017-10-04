a3ed35f40507d3ae1c251bda4db796c73061ad54
Aarthi Raman <aarthiraman@qantas.com.au>
2017-10-04 14:29:49 +1100

    Google and FB logins

--- src/Components/widgets/Login/Login.js
+++ src/Components/widgets/Login/Login.js
@@ -1,5 +1,4 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React from 'react';
 import FacebookLogin from 'react-facebook-login';
 import GoogleLogin from 'react-google-login';
 import styles from './Login.scss';
